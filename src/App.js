fa4abf5d2df68030c1f0b2ddf999ee9248442a60
AbhinavVarre <37716743+AbhinavVarre@users.noreply.github.com>
2022-03-03 02:53:55 -0600

    centered rectangle

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
 function App() {
+    const mystyle = {
+      alignItems: "center",
+
+    };
+  function something()
+  {
+    return (
+      <div>
+        something
+      </div>
+    )
+  }
   return (
     <>
-      <h1 style={{textAlign: "center"}}>  Safety Videos </h1>
-      <svg width="400" height="110">
-        <rect width="300" height="100"  />
-      </svg>
+      <h1 style={{textAlign: "center"}}>Safety Videos</h1>
       <div>
-        <button class = "all-videos" type = "button">All Videos</button>
-        <br>
-        </br>
-        <button class = "my-videos" type = "button">My Videos</button>
+        <div>
+          <button class = "all-videos" type = "button">All Videos</button>
+          <br></br>
+          <button class = "my-videos" type = "button">My Videos</button>
+        </div>
+      <div style = {{display: "flex", justifyContent: "center"}}>
+          <svg width="710" height="710">
+            <rect style = {{color: "red"}} width="600" height="600"/>
+          </svg>
       </div>
+      </div>
+     
     </>
   )
 }

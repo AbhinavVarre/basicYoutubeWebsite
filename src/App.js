8b0f6bc541a1b52143ec7673e3661e3f554402f6
AbhinavVarre <37716743+AbhinavVarre@users.noreply.github.com>
2022-03-03 01:01:40 -0600

    inital app and index file

---
+++ src/App.js
@@ -0,0 +1,18 @@
+function App() {
+  return (
+    <>
+      <h1 style={{textAlign: "center"}}>  Safety Videos </h1>
+      <svg width="400" height="110">
+        <rect width="300" height="100"  />
+      </svg>
+      <div>
+        <button class = "all-videos" type = "button">All Videos</button>
+        <br>
+        </br>
+        <button class = "my-videos" type = "button">My Videos</button>
+      </div>
+    </>
+  )
+}
+
+export default App;

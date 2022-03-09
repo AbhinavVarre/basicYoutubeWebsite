1980e21c11cd276d0393feb4ea64cd476bee846a
eragon1248 <raksheetkota@gmail.com>
2022-03-09 03:01:00 -0600

    first dummy data uploaded

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
+import Home from "./Home";
 import styles from './teststyle.module.css'
 
 function App() {
-  return (
-    <>
-      <h1 style={{textAlign: "center"}}>Safety Videos</h1>
-      <div style={{float: "left"}}>
-            <ul> 
-                <li><a href="" className={styles.active}>My Videos</a></li>
-                <li><a href="">All Videos</a></li>
-            </ul>
+  
+    return (
+      <>
+        <h1 style={{textAlign: "center"}}>Safety Videos</h1>
+        <div style={{float: "left"}}>
+              <ul> 
+                  <li><a href="" className={styles.active}>My Videos</a></li>
+                  <li><a href="">All Videos</a></li>
+              </ul>
+          </div>
+        <div  className={styles.center} style = {{display: "flex", justifyContent: "center"}}>
+          <svg height="600"></svg>
         </div>
-      <div  className={styles.center} style = {{display: "flex", justifyContent: "center"}}>
-        <svg height="600"></svg>
-      </div>
-    </>
-  )
+        <Home/>
+      </>
+    )
 }
 
 export default App;
